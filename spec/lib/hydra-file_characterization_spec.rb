abd53e4e93eba7c6e1a8b3a635361d931247cc74
Blimey74 <jatr@kb.dk>
2013-09-17 13:40:53 -0400

    rspec test in progress for happy path use case

--- spec/lib/hydra-file_characterization_spec.rb
+++ spec/lib/hydra-file_characterization_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 describe Hydra::FileCharacterization do
 
-  Given I have a File on the file system
-  When I call FileCharacterization with the path to the File
-  Then I get a raw XML string
+  #Given I have a File on the file system
+  #When I call FileCharacterization with the path to the File
+  #Then I get a raw XML string
+
+  describe 'characterize file' do
+    subject do
+      @file = File.open(File.join(File.dirname(__FILE__), '..', 'fixtures', "brendan_behan.jpeg"))
+    end 
+    it "returns an XML metadata string for a valid file" do
+      output = double('output')
+      meta_data_xml = Characterizer.characterize(subject)
+    end
+  end 
 
 end
