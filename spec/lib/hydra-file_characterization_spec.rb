0fcee2e534a37745d1b733737b1d0d6bfad4d797
Sue Richeson <spr7b@virginia.edu>
2013-09-17 11:36:19 -0400

    Happy path story

--- spec/lib/hydra-file_characterization_spec.rb
+++ spec/lib/hydra-file_characterization_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe Hydra::FileCharacterization do
 
+  Given I have a File on the file system
+  When I call FileCharacterization with the path to the File
+  Then I get a raw XML string
+
 end
