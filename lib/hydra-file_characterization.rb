485aa9198cee777c5468c853d434b977efce1796
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-17 13:54:58 -0400

    Adding rudimentary characterizer
    This code was lifted from the Hydra::Derivatives and modified to
    ensure closing of the various pipes.

--- lib/hydra-file_characterization.rb
+++ lib/hydra-file_characterization.rb
@@ -1,7 +1,32 @@
 require "hydra-file_characterization/version"
+require 'open3'
 
 module Hydra
   module FileCharacterization
-    # Your code goes here...
+    class Characterizer
+      include Open3
+
+      attr_reader :filename, :fits_path
+      def initialize(filename, fits_path)
+        @filename = filename
+        @fits_path = fits_path
+      end
+
+      def call
+        command = "#{fits_path} -i \"#{filename}\""
+        stdin, stdout, stderr, wait_thr = popen3(command)
+        begin
+          out = stdout.read
+          err = stderr.read
+          exit_status = wait_thr.value
+          raise "Unable to execute command \"#{command}\"\n#{err}" unless exit_status.success?
+          out
+        ensure
+          stdin.close
+          stdout.close
+          stderr.close
+        end
+      end
+    end
   end
 end
--- spec/lib/hydra-file_characterization_spec.rb
+++ spec/lib/hydra-file_characterization_spec.rb
@@ -1,19 +1,17 @@
 require 'spec_helper'
+require 'hydra-file_characterization'
 
-describe Hydra::FileCharacterization do
+describe Hydra::FileCharacterization::Characterizer do
+  def fixture_file(filename)
+    File.expand_path(File.join('../../fixtures', filename), __FILE__)
+  end
 
-  #Given I have a File on the file system
-  #When I call FileCharacterization with the path to the File
-  #Then I get a raw XML string
+  let(:filename) { fixture_file('brendan_behan.jpeg') }
+  let(:fits_path) { `which fits`.strip }
+  subject { Hydra::FileCharacterization::Characterizer.new(filename, fits_path) }
 
-  describe 'characterize file' do
-    subject do
-      @file = File.open(File.join(File.dirname(__FILE__), '..', 'fixtures', "brendan_behan.jpeg"))
-    end 
-    it "returns an XML metadata string for a valid file" do
-      output = double('output')
-      meta_data_xml = Characterizer.characterize(subject)
-    end
-  end 
+  it '#call' do
+    expect(subject.call).to include(%(<identity format="JPEG File Interchange Format" mimetype="image/jpeg"))
+  end
 
 end
