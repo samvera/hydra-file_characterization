c56244a3fc8cdb641e525ba146d33468de51e357
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-18 10:47:29 -0400

    Extracting Character spec to its own file

--- spec/lib/hydra-file_characterization_spec.rb
+++ spec/lib/hydra-file_characterization_spec.rb
@@ -17,39 +17,4 @@ describe Hydra::FileCharacterization do
     end
   end
 
-  describe 'Characterizer' do
-    def fixture_file(filename)
-      File.expand_path(File.join('../../fixtures', filename), __FILE__)
-    end
-
-    let(:fits_path) { `which fits || which fits.sh`.strip }
-    subject { Hydra::FileCharacterization::Characterizer.new(filename, fits_path) }
-
-    describe 'validfile' do
-      let(:filename) { fixture_file('brendan_behan.jpeg') }
-      it '#call' do
-        expect(subject.call).to include(%(<identity format="JPEG File Interchange Format" mimetype="image/jpeg"))
-      end
-    end
-
-    describe 'invalidFile' do
-      let(:filename) { fixture_file('nofile.pdf') }
-      it "should raise an error if the path does not contain the file" do
-        expect {subject.call}.to raise_error(Hydra::FileCharacterization::Characterizer::FileNotFoundError)
-      end
-    end
-
-    describe 'corruptFile' do
-      let(:filename) { fixture_file('brendan_broken.dxxd') }
-      it "should return xml showing Unknown Binary and application/octet-stream mimetype" do
-        expect(subject.call).to include(%(<identity format="Unknown Binary" mimetype="application/octet-stream"))
-      end
-    end
-
-    describe 'zip file should be characterized not its contents' do
-      let(:filename) { fixture_file('archive.zip') }
-      its(:call) { should include(%(<identity format="ZIP Format" mimetype="application/zip"))}
-    end
-
-  end
 end
