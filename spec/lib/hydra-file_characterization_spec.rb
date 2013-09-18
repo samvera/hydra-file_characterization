8a1c1573d1fef592a5071604335ab12e60215fcc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-18 09:57:25 -0400

    Adding spec for ToTempFile behavior

--- spec/lib/hydra-file_characterization_spec.rb
+++ spec/lib/hydra-file_characterization_spec.rb
@@ -1,38 +1,55 @@
 require 'spec_helper'
 require 'hydra-file_characterization'
 
-describe Hydra::FileCharacterization::Characterizer do
-  def fixture_file(filename)
-    File.expand_path(File.join('../../fixtures', filename), __FILE__)
-  end
+describe Hydra::FileCharacterization do
 
-  let(:fits_path) { `which fits || which fits.sh`.strip }
-  subject { Hydra::FileCharacterization::Characterizer.new(filename, fits_path) }
+  describe Filer do
+    subject(Hydra::FileCharacterization::ToTempFile.new(string, "hello.rb"))
 
-  describe 'validfile' do
-    let(:filename) { fixture_file('brendan_behan.jpeg') }
-    it '#call' do
-      expect(subject.call).to include(%(<identity format="JPEG File Interchange Format" mimetype="image/jpeg"))
+    it 'create a tempfile that exists' do
+      @tempfile = nil
+      subject.call do |temp_file|
+        @temp_file = temp_file
+        expect(File.exist?(@temp_file)).to eq true
+        expect(File.extname(@temp_file)).to eq '.rb'
+      end
+      expect(File.exist?(@temp_file)).to eq false
     end
   end
 
-  describe 'invalidFile' do
-    let(:filename) { fixture_file('nofile.pdf') }
-    it "should raise an error if the path does not contain the file" do
-      expect {subject.call}.to raise_error(Hydra::FileCharacterization::Characterizer::FileNotFoundError)
+  describe Characterizer do
+    def fixture_file(filename)
+      File.expand_path(File.join('../../fixtures', filename), __FILE__)
     end
-  end
 
-  describe 'corruptFile' do
-    let(:filename) { fixture_file('brendan_broken.dxxd') }
-    it "should return xml showing Unknown Binary and application/octet-stream mimetype" do
-      expect(subject.call).to include(%(<identity format="Unknown Binary" mimetype="application/octet-stream"))
+    let(:fits_path) { `which fits || which fits.sh`.strip }
+    subject { Hydra::FileCharacterization::Characterizer.new(filename, fits_path) }
+
+    describe 'validfile' do
+      let(:filename) { fixture_file('brendan_behan.jpeg') }
+      it '#call' do
+        expect(subject.call).to include(%(<identity format="JPEG File Interchange Format" mimetype="image/jpeg"))
+      end
     end
-  end
 
-  describe 'zip file should be characterized not its contents' do
-    let(:filename) { fixture_file('archive.zip') }
-    its(:call) { should include(%(<identity format="ZIP Format" mimetype="application/zip"))}
-  end
+    describe 'invalidFile' do
+      let(:filename) { fixture_file('nofile.pdf') }
+      it "should raise an error if the path does not contain the file" do
+        expect {subject.call}.to raise_error(Hydra::FileCharacterization::Characterizer::FileNotFoundError)
+      end
+    end
 
+    describe 'corruptFile' do
+      let(:filename) { fixture_file('brendan_broken.dxxd') }
+      it "should return xml showing Unknown Binary and application/octet-stream mimetype" do
+        expect(subject.call).to include(%(<identity format="Unknown Binary" mimetype="application/octet-stream"))
+      end
+    end
+
+    describe 'zip file should be characterized not its contents' do
+      let(:filename) { fixture_file('archive.zip') }
+      its(:call) { should include(%(<identity format="ZIP Format" mimetype="application/zip"))}
+    end
+
+  end
 end
