d057d32a4ffe453efa5b18da4bdbaf5d560755fb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-18 10:43:27 -0400

    Adding explicit require for active_support

--- lib/hydra-file_characterization.rb
+++ lib/hydra-file_characterization.rb
@@ -1,10 +1,10 @@
 require "hydra-file_characterization/version"
 require "hydra-file_characterization/characterizer"
+require "active_support/configurable"
 
 module Hydra
   module FileCharacterization
     include ActiveSupport::Configurable
-
   end
 end
 
