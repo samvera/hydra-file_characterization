015b4cb107ade30e17b53f5fd6f72c7dad8af489
Blimey74 <jatr@kb.dk>
2013-09-18 10:42:08 -0400

    Creating new Characterizer class

--- lib/hydra-file_characterization.rb
+++ lib/hydra-file_characterization.rb
@@ -1,39 +1,10 @@
 require "hydra-file_characterization/version"
-require 'open3'
+require "hydra-file_characterization/characterizer"
 
 module Hydra
   module FileCharacterization
-    class Characterizer
-      include Open3
+    include ActiveSupport::Configurable
 
-      class FileNotFoundError < RuntimeError
-      end
-
-      attr_reader :filename, :fits_path
-      def initialize(filename, fits_path)
-        @filename = filename
-        @fits_path = fits_path
-      end
-
-      def call
-        unless File.exists?(filename)
-          raise FileNotFoundError.new("File: #{filename} does not exist.")
-        end
-        command = "#{fits_path} -i \"#{filename}\""
-        stdin, stdout, stderr, wait_thr = popen3(command)
-        begin
-          out = stdout.read
-          err = stderr.read
-          exit_status = wait_thr.value
-          raise "Unable to execute command \"#{command}\"\n#{err}" unless exit_status.success?
-          out
-        ensure
-          stdin.close
-          stdout.close
-          stderr.close
-        end
-      end
-    end
   end
 end
 
---
+++ lib/hydra-file_characterization/characterizer.rb
@@ -0,0 +1,36 @@
+require 'open3'
+
+module Hydra::FileCharacterization
+
+  class Characterizer
+    include Open3
+
+    class FileNotFoundError < RuntimeError
+    end
+
+    attr_reader :filename, :fits_path
+    def initialize(filename, fits_path)
+      @filename = filename
+      @fits_path = fits_path
+    end
+
+    def call
+      unless File.exists?(filename)
+        raise FileNotFoundError.new("File: #{filename} does not exist.")
+      end
+      command = "#{fits_path} -i \"#{filename}\""
+      stdin, stdout, stderr, wait_thr = popen3(command)
+      begin
+        out = stdout.read
+        err = stderr.read
+        exit_status = wait_thr.value
+        raise "Unable to execute command \"#{command}\"\n#{err}" unless exit_status.success?
+        out
+      ensure
+        stdin.close
+        stdout.close
+        stderr.close
+      end
+    end
+  end
+end
