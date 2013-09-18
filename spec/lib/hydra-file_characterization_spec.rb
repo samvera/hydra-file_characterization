30e6c6b25c463783363dc2d696d6666f8be0abc4
Sue Richeson <spr7b@virginia.edu>
2013-09-18 16:10:12 -0400

    Add combined ToTempFile and characterizer test.

--- spec/lib/hydra-file_characterization_spec.rb
+++ spec/lib/hydra-file_characterization_spec.rb
@@ -3,8 +3,6 @@ require 'hydra-file_characterization'
 
 describe Hydra::FileCharacterization do
 
-
-
   describe 'config' do
     subject { Hydra::FileCharacterization::Configuration.new }
     let (:expected_fits_path) {"string"}
@@ -13,6 +11,17 @@ describe Hydra::FileCharacterization do
     end
     its(:config) {should have_key :fits_path}
     its(:fits_path) {should == expected_fits_path}
+  end
+
+  describe 'arbitrary stream of characters' do
+    let (:tempfile) { Hydra::FileCharacterization::ToTempFile.new("This is the content of the file.", 'test.rb')}
+    let(:fits_path) { `which fits || which fits.sh`.strip }
+    it '#call' do
+      tempfile.call do |f|
+        @fits_output = Hydra::FileCharacterization::Characterizers::Fits.new(f.path,fits_path ).call
+      end
+      expect(@fits_output).to include '<identity format="Plain text" mimetype="text/plain"'
+    end
   end
 
 end
