ef3efaded695cd34af7000336c7f579aaa6f7ffb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-19 14:14:16 -0400

    Adding documentation for Hydra

--- lib/hydra/file_characterization.rb
+++ lib/hydra/file_characterization.rb
@@ -8,9 +8,23 @@ require "active_support/configurable"
 module Hydra
 
   module_function
-  def characterize(content, filename, *options)
+
+  #
+  # Run all of the specified tools against the given content and filename.
+  #
+  # @param [String] content - The contents of the original file
+  # @param [String] filename - The original file's filename; Some
+  #   characterization tools take hints from the file names
+  # @param [Array] tool_names - A list of tool names available on the system
+  #
+  # @return [String, Array<String>] -
+  #    String - When a single tool_name is given, returns the raw XML as a
+  #      string
+  #    Array<String> - When multiple tool_names are given, returns an equal
+  #      length Array of XML strings
+  def characterize(content, filename, *tool_names)
     tool_outputs = []
-    tool_names = Array(options).flatten.compact
+    tool_names = Array(tool_names).flatten.compact
     FileCharacterization::ToTempFile.open(content, filename) do |f|
       tool_names.each do |tool_name|
         tool_outputs << FileCharacterization.characterize_with(tool_name, f.path)
