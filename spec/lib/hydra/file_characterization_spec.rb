43ccd64e8b84f6bb46e2a584651b192114396f43
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-19 08:25:14 -0400

    Factoring specs to remove some namespace chatter

--- spec/lib/hydra/file_characterization_spec.rb
+++ spec/lib/hydra/file_characterization_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 require 'hydra/file_characterization'
 
-describe Hydra::FileCharacterization do
+module Hydra::FileCharacterization
 
-  describe 'config' do
-    subject { Hydra::FileCharacterization::Configuration.new }
+  describe Configuration do
+    subject { Configuration.new }
     let (:expected_fits_path) {"string"}
     before(:each) do
       subject.fits_path = expected_fits_path
@@ -13,12 +13,12 @@ describe Hydra::FileCharacterization do
     its(:fits_path) {should == expected_fits_path}
   end
 
-  describe 'arbitrary stream of characters' do
-    let (:tempfile) { Hydra::FileCharacterization::ToTempFile.new("This is the content of the file.", 'test.rb')}
+  describe 'preliminary integration' do
+    let (:tempfile) { ToTempFile.new("This is the content of the file.", 'test.rb')}
     let(:fits_path) { `which fits || which fits.sh`.strip }
     it '#call' do
       tempfile.call do |f|
-        @fits_output = Hydra::FileCharacterization::Characterizers::Fits.new(f.path,fits_path ).call
+        @fits_output = Characterizers::Fits.new(f.path,fits_path ).call
       end
       expect(@fits_output).to include '<identity format="Plain text" mimetype="text/plain"'
     end
