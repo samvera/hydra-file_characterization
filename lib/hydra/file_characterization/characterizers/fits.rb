935d18d865b58f1f0bb3364be7594a02636effb2
Justin Coyne <jcoyne@justincoyne.com>
2015-10-15 12:49:49 -0500

    Handle non-xml output from FITS
    This logs a warning with whatever messages preceed the <?xml> header
    See https://github.com/harvard-lts/fits/issues/46

--- lib/hydra/file_characterization/characterizers/fits.rb
+++ lib/hydra/file_characterization/characterizers/fits.rb
@@ -1,5 +1,6 @@
 require 'hydra/file_characterization/exceptions'
 require 'hydra/file_characterization/characterizer'
+require 'logger'
 module Hydra::FileCharacterization::Characterizers
   class Fits < Hydra::FileCharacterization::Characterizer
 
@@ -9,10 +10,14 @@ module Hydra::FileCharacterization::Characterizers
         "#{tool_path} -i \"#{filename}\""
       end
 
-      # Remove any residual non-XML from JHOVE
+      # Remove any non-XML output that precedes the <?xml> tag
       # See: https://github.com/harvard-lts/fits/issues/20
+      #      https://github.com/harvard-lts/fits/issues/40
+      #      https://github.com/harvard-lts/fits/issues/46
       def post_process(raw_output)
-        raw_output.sub(/^READBOX seen=true\n/, '')
+        md = /\A(.*)(<\?xml.*)\Z/m.match(raw_output)
+        logger.warn "FITS produced non-xml output: \"#{md[1].chomp}\"" unless md[1].empty?
+        md[2]
       end
   end
 end
